5a1623e28df4c5cfdc10fd5fe90bc3b587e55fb9
JoDaUT <jose.david.urena.torres@gmail.com>
2023-02-13 01:08:10 -0600

    feat(master): Add associations into furnance and qualityCheckDevice

--- database/models/furnance.js
+++ database/models/furnance.js
@@ -9,11 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Furnance.belongsTo(models.Device, { foreignKey: "deviceId" });
     }
   }
   Furnance.init(
     {
-      maxTemperture: { type: DataTypes.FLOAT, allowNull: false },
+      maxTemperature: { type: DataTypes.FLOAT, allowNull: false },
     },
     {
       sequelize,
--- database/models/qualitycheckdevice.js
+++ database/models/qualitycheckdevice.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class QualityCheckDevice extends Model {
     /**
@@ -11,13 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      QualityCheckDevice.belongsTo(models.Device, { foreignKey: "deviceId" });
     }
   }
-  QualityCheckDevice.init({
-    capacity: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'QualityCheckDevice',
-  });
+  QualityCheckDevice.init(
+    {
+      capacity: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "QualityCheckDevice",
+    }
+  );
   return QualityCheckDevice;
 };
