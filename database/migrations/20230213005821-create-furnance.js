0d745d3123ff9f43bf26469c008afb58b063195b
JoDaUT <jose.david.urena.torres@gmail.com>
2023-02-13 01:11:36 -0600

    fix(master): Typo in furnance migration

--- database/migrations/20230213005821-create-furnance.js
+++ database/migrations/20230213005821-create-furnance.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      maxTemperture: {
+      maxTemperature: {
         type: Sequelize.FLOAT,
         allowNull: false,
       },
