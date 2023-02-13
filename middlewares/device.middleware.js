e1da5aa835ffd5dec3c4b6b03e6e6482d29c15a4
JoDaUT <jose.david.urena.torres@gmail.com>
2023-02-12 23:58:23 -0600

    feat(master): Validate the status in the device middleware

--- middlewares/device.middleware.js
+++ middlewares/device.middleware.js
@@ -2,7 +2,6 @@ const { Device } = require("../database/models");
 
 async function deviceExists(req, res, next) {
   const { name, factoryId, ip } = req.body;
-  console.log({ name, factoryId, ip });
   const device = await Device.findOne({
     where: { name, ip },
   });
@@ -15,12 +14,16 @@ async function deviceExists(req, res, next) {
 }
 
 async function isValidDevice(req, res, next) {
-  const { ip } = req.body;
+  const { ip, status } = req.body;
   const validIp =
     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
       ip
     );
   if (!validIp) {
+    return res.status(400).json({ errors: ["Invalid device data"] });
+  }
+
+  if (!["Online", "Offline"].includes(status)) {
     return res.status(400).json({ errors: ["Invalid device data"] });
   }
   next();
